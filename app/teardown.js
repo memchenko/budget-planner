a799f237983ac177a5c5a251721fc8bc41caad3d
memchenko <git config --global user.email you@example.com>
2022-12-13 01:38:04 +0100

    safe teardown

--- app/teardown.js
+++ app/teardown.js
@@ -1,24 +1,29 @@
-const { info } = require("./lib/log");
+const { info, error } = require("./lib/log");
 const user = require("./entities/user");
 const gui = require("./modules/gui");
 
 async function teardown() {
   info("Begin graceful teardown");
 
-  const users = await user.findAll();
+  try {
+    const users = await user.findAll();
 
-  await Promise.all(
-    users.map(({ tgId }) => {
-      return gui.respondWithMessage({
-        userId: tgId,
-        text: "Бот временно не работает. Ожидайте уведомления о восстановлении работоспособности. Спасибо!",
-      });
-    })
-  );
+    await Promise.all(
+      users.map(({ tgId }) => {
+        return gui.respondWithMessage({
+          userId: tgId,
+          text: "Бот временно не работает. Ожидайте уведомления о восстановлении работоспособности. Спасибо!",
+        });
+      })
+    );
+  } catch (err) {
+    error("Failed to graceful teardown");
+    error(err);
+  } finally {
+    info("End graceful teardown");
 
-  info("End graceful teardown");
-
-  process.exit(0);
+    process.exit(0);
+  }
 }
 
 module.exports = {
