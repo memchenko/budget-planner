ae81deb378c83c3c05ffa160dc64af0e03e3a0ce
memchenko <git config --global user.email you@example.com>
2025-05-04 00:06:32 +0200

    fix camera

--- apps/pwa/src/components/features/webrtc-connection/components/qr-reader/view.tsx
+++ apps/pwa/src/components/features/webrtc-connection/components/qr-reader/view.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { observer } from 'mobx-react-lite';
 import feather from 'feather-icons';
+import cn from 'classnames';
 import { Input } from '@nextui-org/input';
 import { useController } from '~/shared/hooks/useController';
-// import { useUnmount } from '~/shared/hooks/useUnmount';
 import { Button } from '~/components/ui/button';
+import { isMobile } from '~/shared/utils/device';
 
 import { QRReaderController } from './controller';
+import styles from './styles.module.css';
 
 export interface QrReaderProps extends PropsWithChildren<unknown> {
   onRead: (data: string) => void;
@@ -16,12 +18,15 @@ export const QrReader = observer(({ onRead }: QrReaderProps) => {
   const ctrl = useController(QRReaderController);
   ctrl.onRead = onRead;
 
-  // useUnmount(ctrl.reset);
-
   return (
     <div className="flex flex-col gap-4 items-end sm:items-center">
       <div className="flex items-center w-full aspect-square sm:aspect-video sm:max-w-[900px] overflow-hidden justify-center bg-black">
-        <video className="h-full max-w-none" ref={(elem) => ctrl.setVideoElement(elem)}></video>
+        <video
+          className={cn(styles.video, {
+            [styles.desktopVideo]: !isMobile,
+          })}
+          ref={(elem) => ctrl.setVideoElement(elem)}
+        ></video>
       </div>
       <Button fullWidth as="label" className="relative overflow-hidden">
         <span dangerouslySetInnerHTML={{ __html: feather.icons.image.toSvg() }} />
